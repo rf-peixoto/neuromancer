72a564478d6164fd4e39648cfae1b8125b96c746
Corvo <50427765+rf-peixoto@users.noreply.github.com>
2022-08-10 12:28:59 -0300

    Update neuromancer.py

--- neuromancer.py
+++ neuromancer.py
@@ -3,7 +3,7 @@ import json, sys
 # ----------------------------------------------------------------------------- #
 # Prepare Class:
 class Neuromancer:
-    def __init__(self, filename, tolerance=5):
+    def __init__(self, filename, tolerance=15):
         self.filename = filename
         self.file_hash = ""
         self.tolerance = tolerance
